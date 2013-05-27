90f879d131b44a899227cb285788a2208b47645b
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-05-27 21:48:22 +0300

    fix

--- parser/parser.go
+++ parser/parser.go
@@ -3,6 +3,9 @@ package parser
 import (
   "bytes"
   "net/mail"
+  "mime"
+  "mime/multipart"
+  "io"
   "io/ioutil"
   "time"
   "github.com/le0pard/go-falcon/log"
@@ -58,10 +61,66 @@ func (email *ParsedEmail) parseEmailHeaders(msg *mail.Message) {
   }
 }
 
+// parse plain email
+
+func (email *ParsedEmail) parsePlainEmail() {
+  contentType := email.Headers.Get("Content-Type")
+  log.Debugf("ContentType: %v", contentType)
+}
+
+// parse plain email
+
+func (email *ParsedEmail) parseMimeEmail() {
+  contentType := email.Headers.Get("Content-Type")
+  log.Debugf("ContentType: %v", contentType)
+  contentTypeVal, contentTypeParams, err := mime.ParseMediaType(contentType)
+  if err != nil {
+    log.Errorf("Invalid ContentType: %v", err)
+    return
+  }
+  log.Debugf("ContentType Value: %v", contentTypeVal)
+  log.Debugf("ContentType params: %v", contentTypeParams)
+  if contentTypeParams["boundary"] == "" {
+    log.Errorf("No boundary: %v", contentTypeParams)
+    return
+  }
+  buf := new(bytes.Buffer)
+  bodyReader := bytes.NewReader(email.EmailBody)
+  reader := multipart.NewReader(bodyReader, contentTypeParams["boundary"])
+
+  for {
+    p, err := reader.NextPart()
+    if err == io.EOF {
+      break
+    }
+    if err != nil {
+      log.Errorf("NextPart: %v", err)
+    }
+    pbody, err := ioutil.ReadAll(p)
+
+  }
+
+  part, err := reader.NextPart()
+  if part == nil || err != nil {
+    log.Errorf("No boundary: %v", contentTypeParams)
+    return
+  }
+  if _, err := io.Copy(buf, part); err != nil {
+    log.Errorf("Error: %v", err)
+  }
+  log.Debugf("Part: %v", buf.String())
+}
+
 // parse body
 
 func (email *ParsedEmail) parseEmailBody(msg *mail.Message, body []byte) {
   email.EmailBody = body
+  mimeVersion := email.Headers.Get("Mime-Version")
+  if mimeVersion != "" {
+    email.parseMimeEmail()
+  } else {
+    email.parsePlainEmail()
+  }
 }
 
 // obj
@@ -86,5 +145,5 @@ func (parser *EmailParser) ParseMail(env *smtpd.BasicEnvelope) {
   email.RawMail = email.env.MailBody
   email.parseEmailHeaders(msg)
   email.parseEmailBody(msg, mailBody)
-  log.Debugf("parsed: %v", email)
+  //log.Debugf("parsed: %v", email)
 }
