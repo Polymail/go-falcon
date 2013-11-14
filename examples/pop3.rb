a714797622dbea503f8680602cfdb2bf270a22ce
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-11-14 23:54:10 +0200

    pop3

--- examples/pop3.rb
+++ examples/pop3.rb
@@ -1,15 +1,15 @@
 require 'net/pop'
 
-Net::POP3.start('falcon.rw.rw', 110, '90681fe5f57d5', '3e526ed313d8dc', true) do |pop|
+raise "args shoud be email ans pass" if ARGV.length < 2
+username, password = ARGV[0], ARGV[1]
+
+Net::POP3.start('falcon.rw.rw', 110, username, password, true) do |pop|
   if pop.mails.empty?
     puts 'No mail.'
   else
-    i = 0
     pop.each_mail do |m|   # or "pop.mails.each ..."   # (2)
       puts m.pop
       m.delete
-      break if i > 1
-      i += 1
     end
     puts "#{pop.mails.size} mails popped."
   end
