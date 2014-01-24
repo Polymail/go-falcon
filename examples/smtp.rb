c021a29d0f18c7af393847db3f508404855829d7
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-01-24 17:23:53 +0200

    spam

--- examples/smtp.rb
+++ examples/smtp.rb
@@ -15,6 +15,35 @@ END
 
 =begin
 message = <<-END.split("\n").map!(&:strip).join("\n")
+Subject: Test spam mail (GTUBE)
+Message-ID: <GTUBE1.1010101@example.net>
+Date: Wed, 23 Jul 2003 23:30:00 +0200
+From: Sender <sender@example.net>
+To: Recipient <recipient@example.net>
+Precedence: junk
+MIME-Version: 1.0
+Content-Type: text/plain; charset=us-ascii
+Content-Transfer-Encoding: 7bit
+
+This is the GTUBE, the
+	Generic
+	Test for
+	Unsolicited
+	Bulk
+	Email
+
+If your spam filter supports it, the GTUBE provides a test by which you
+can verify that the filter is installed correctly and is detecting incoming
+spam. You can send yourself a test mail containing the following string of
+characters (in upper case and with no white spaces and line breaks):
+
+XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X
+
+You should send this test mail from an account outside of your network.
+END
+
+
+message = <<-END.split("\n").map!(&:strip).join("\n")
 From: Private Person <me@fromdomain.com>
 To: A Test User <test@todomain.com>
 Subject: Virus message
@@ -51,7 +80,7 @@ Net::SMTP.start('falcon.rw.rw',
                               ['test@todomain.com', 'test2@todomain.com', 'test3@todomain.com']
 end
 =end
-=begin
+#=begin
 arr = []
 1.times do
   arr << Thread.new do
@@ -69,8 +98,8 @@ arr = []
 end
 
 arr.each {|t| t.join }
-=end
-#=begin
+#=end
+=begin
 
   Net::SMTP.start('localhost',
                   2525,
@@ -95,4 +124,4 @@ arr.each {|t| t.join }
                                 ['test@todomain.com', 'test2@todomain.com', 'test3@todomain.com']
   end
 
-#=end
+=end
