b3cfc340ef182a135a04577a9da342b63dbd9654
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-09-22 16:42:16 -0400

    smtp

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
   "github.com/le0pard/go-falcon/log"
   "github.com/le0pard/go-falcon/protocol"
   "github.com/le0pard/go-falcon/proxy"
+  "fmt"
+  "net/http"
+  _ "net/http/pprof"
 )
 
 var (
@@ -21,7 +24,13 @@ func main() {
   // conf
   log.Debugf("Loaded config: %+v", globalConfig)
   // start nginx proxy
-  proxy.StartNginxHTTPProxy(globalConfig)
+  if globalConfig.Proxy.Enabled {
+    proxy.StartNginxHTTPProxy(globalConfig)
+  } else {
+    go func() {
+      log.Infof("%+v", http.ListenAndServe(fmt.Sprintf("%s:%d", globalConfig.Adapter.Host, globalConfig.Adapter.Port + 2000), nil))
+    }()
+  }
   // start pop3 server
   protocol.StartPop3Server(globalConfig)
   // start smtp server
